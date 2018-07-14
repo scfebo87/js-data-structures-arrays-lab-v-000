230ebd56e23757ee283189eec85a48b3b80fb02f
Sean Christopher Febo <scf@techie.com>
2018-07-14 23:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const drivers =  ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   drivers.push(name);
 }
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
